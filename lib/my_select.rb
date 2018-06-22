15c50b15725a33f859def9ecefac5889d3666761
William Chabla <w_chabla@hotmail.com>
2018-06-22 02:42:20 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+  #if collection == []
+  #  raise "This block should not run!"
+    if block_given?
+      i = 0
+      new_array = []
+      
+      while i < collection.length do
+        #new_array << yield(collection[i])
+        if yield(collection[i]) == true
+          new_array << collection[i]
+        end
+        i += 1
+      end
+      new_array
+    else
+      nil
+  end
 end
